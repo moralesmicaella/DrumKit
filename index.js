e157fe34f93bb4badc41001d293c5c9bea7fe21e
Micaella Morales <morales.micaella@gmail.com>
2022-01-19 16:37:25 -0800

    Refactor audio code

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 
 var drumButtons = document.querySelectorAll(".drum");
 
-var tom1Sound = new Audio("sounds/tom-1.mp3");
-var tom2Sound = new Audio("sounds/tom-2.mp3");
-var tom3Sound = new Audio("sounds/tom-3.mp3");
-var tom4Sound = new Audio("sounds/tom-4.mp3");
-var snareSound = new Audio("sounds/snare.mp3");
-var crashSound = new Audio("sounds/crash.mp3");
-var kickBassSound = new Audio("sounds/kick-bass.mp3");
-
 // Detecting Button Press
 for (let drumButton of drumButtons) {
   drumButton.addEventListener("click", function () {
@@ -26,24 +18,31 @@ document.addEventListener("keydown", function (event) {
 function playDrumSound(key) {
   switch (key) {
     case "w":
+      var tom1Sound = new Audio("sounds/tom-1.mp3");
       tom1Sound.play();
       break;
     case "a":
+      var tom2Sound = new Audio("sounds/tom-2.mp3");
       tom2Sound.play();
       break;
     case "s":
+      var tom3Sound = new Audio("sounds/tom-3.mp3");
       tom3Sound.play();
       break;
     case "d":
+      var tom4Sound = new Audio("sounds/tom-4.mp3");
       tom4Sound.play();
       break;
     case "j":
+      var snareSound = new Audio("sounds/snare.mp3");
       snareSound.play();
       break;
     case "k":
+      var crashSound = new Audio("sounds/crash.mp3");
       crashSound.play();
       break;
     case "l":
+      var kickBassSound = new Audio("sounds/kick-bass.mp3");
       kickBassSound.play();
       break;
 
